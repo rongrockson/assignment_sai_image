5acb671865dc986e6d3ee33dba18bbad6564b6f6
rongrockson <rocksonrong@gmail.com>
2024-08-31 14:58:14 +0530

    image is now uplaoded to s3 with 50 percent quality

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -10,102 +10,102 @@ const { s3 } = require('../config/config');
 const logger = require('../config/logger');
 
 const downloadImage = async (url) => {
-    const response = await axios({
-        url,
-        responseType: 'arraybuffer',
-    });
+  const response = await axios({
+    url,
+    responseType: 'arraybuffer',
+  });
 
-    return Buffer.from(response.data, 'binary');
+  return Buffer.from(response.data, 'binary');
 };
 
 const saveProducts = async (products) => {
-    await Product.insertMany(products);
+  await Product.insertMany(products);
 };
 
 const uploadToS3 = async (buffer, filename) => {
-    try {
-        const params = {
-            Bucket: process.env.S3_BUCKET_NAME,
-            Key: filename,
-            Body: buffer,
-            ContentType: 'image/jpeg', // Adjust the content type if needed
-        };
-        const data = await s3.upload(params).promise();
-        logger.info('File uploaded to S3', data.Location);
-        // Returns the URL of the uploaded file
-        return data.Location;
-    } catch (error) {
-        logger.error('Error uploading file to S3', error);
-        throw new Error('Error uploading file to S3');
-    }
+  try {
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: filename,
+      Body: buffer,
+      ContentType: 'image/jpeg', // Adjust the content type if needed
+    };
+    const data = await s3.upload(params).promise();
+    logger.info('File uploaded to S3', data.Location);
+    // Returns the URL of the uploaded file
+    return data.Location;
+  } catch (error) {
+    logger.error('Error uploading file to S3', error);
+    throw new Error('Error uploading file to S3');
+  }
 };
 
 const processImage = async (inputUrl) => {
-    try {
-        const imageBuffer = await downloadImage(inputUrl);
-        const filename = `${uuidv4()}${path.extname(inputUrl)}`;
-        logger.info(`Processing image ${inputUrl} with filename ${filename}`);
-        // reduce the quality of the image to 50%
-        const processedBuffer = await sharp(imageBuffer)
-            .jpeg({ quality: 50 })
-            .png({ quality: 50 })
-            .webp({ quality: 50 })
-            .toBuffer();
-        const s3Url = await uploadToS3(processedBuffer, filename);
+  try {
+    const imageBuffer = await downloadImage(inputUrl);
+    const filename = `${uuidv4()}${path.extname(inputUrl)}`;
+    logger.info(`Processing image ${inputUrl} with filename ${filename}`);
+    // reduce the quality of the image to 50%
+    const processedBuffer = await sharp(imageBuffer)
+      .jpeg({ quality: 50 })
+      .png({ quality: 50 })
+      .webp({ quality: 50 })
+      .toBuffer();
+    const s3Url = await uploadToS3(processedBuffer, filename);
 
-        return s3Url;
-    } catch (error) {
-        logger.error(`Error processing image ${inputUrl}`, error);
-        return null;
-    }
+    return s3Url;
+  } catch (error) {
+    logger.error(`Error processing image ${inputUrl}`, error);
+    return null;
+  }
 };
 
 const processProducts = async (requestId) => {
-    const products = await Product.find({ requestId });
+  const products = await Product.find({ requestId });
 
-    logger.info(`Processing ${products.length} products`);
+  logger.info(`Processing ${products.length} products`);
 
-    const promises = products.map((product) =>
-        Promise.all(product.inputUrls.map(processImage)).then((outputUrls) => {
-            return Product.findByIdAndUpdate(
-                product._id,
-                {
-                    outputUrls,
-                    processedAt: new Date(),
-                },
-                { new: true }
-            );
-        })
-    );
+  const promises = products.map((product) =>
+    Promise.all(product.inputUrls.map(processImage)).then((outputUrls) => {
+      return Product.findByIdAndUpdate(
+        product._id,
+        {
+          outputUrls,
+          processedAt: new Date(),
+        },
+        { new: true }
+      );
+    })
+  );
 
-    await Promise.allSettled(promises);
+  await Promise.allSettled(promises);
 };
 
 const generateCsv = async (requestId) => {
-    const products = await Product.find({ requestId });
-    if (!products || products.length === 0) {
-        throw new ApiError(httpStatus.NOT_FOUND, 'No products found for this request');
-    }
+  const products = await Product.find({ requestId });
+  if (!products || products.length === 0) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'No products found for this request');
+  }
 
-    const fields = ['S. No.', 'Product Name', 'Input Image Urls', 'Output Image Urls'];
-    const data = products.map((product) => ({
-        'S. No.': product.serialNumber,
-        'Product Name': product.name,
-        'Input Image Urls': product.inputUrls.join(', '),
-        'Output Image Urls': product.outputUrls.join(', '),
-    }));
+  const fields = ['S. No.', 'Product Name', 'Input Image Urls', 'Output Image Urls'];
+  const data = products.map((product) => ({
+    'S. No.': product.serialNumber,
+    'Product Name': product.name,
+    'Input Image Urls': product.inputUrls.join(', '),
+    'Output Image Urls': product.outputUrls.join(', '),
+  }));
 
-    const parser = new Parser({ fields, delimiter: '|' });
-    const csvData = parser.parse(data);
+  const parser = new Parser({ fields, delimiter: '|' });
+  const csvData = parser.parse(data);
 
-    return {
-        csvData: csvData,
-        filename: `processed_product_images_${requestId}.csv`,
-    };
+  return {
+    csvData,
+    filename: `processed_product_images_${requestId}.csv`,
+  };
 };
 
 module.exports = {
-    saveProducts,
-    processProducts,
-    generateCsv,
+  saveProducts,
+  processProducts,
+  generateCsv,
 };
