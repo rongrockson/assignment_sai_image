ed1c312458d7bd17698f6fcd23e76af4b991af8b
rongrockson <rocksonrong@gmail.com>
2024-08-31 21:28:23 +0530

    webhook workflow

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -3,18 +3,22 @@ const axios = require('axios');
 const catchAsync = require('../utils/catchAsync');
 const imagesService = require('../services/images.service');
 const config = require('../config/config');
+const logger = require('../config/logger');
 
 // call the main service after processing the images
 const sendOutput = async (requestId, serialNumber, outputUrls) => {
   try {
-    const response = await axios.post(`${config.mainServiceUrl}/v1/product-image-csv/webhook`, {
+    const url = `${config.mainServiceUrl}/v1/product-image-csv/webhook`;
+    logger.info(`Sending output to main service ${url}`);
+    const response = await axios.post(`${url}`, {
       requestId,
       serialNumber,
       outputUrls,
     });
     return response.data;
   } catch (error) {
-    throw new Error('Error sending output to main service');
+    logger.error('Error sending output to main service', error);
+    throw new Error('Error sending output to main service', error);
   }
 };
 
